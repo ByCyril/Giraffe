d12788199cd4ab694f5d534de4cef7c63bd8abe8
Cyril Garcia <garciacyrilivar5768@yahoo.com>
2019-05-28 16:36:49 -0700

    finished confidence intervals

--- simple_linear_regression.js
+++ simple_linear_regression.js
@@ -74,13 +74,16 @@ function sampleCorrelation() {
   return ab.div(aa.mul(bb).sqrt());
 }
 
-function confidenceInterval(t) {
+// Confidence interval for the regression slope
+function confidenceIntervalForSlope(t) {
   const n = xData.shape[0];
   const mse = sse.div(tf.scalar(n - 2));
+
   const sxx = xData
     .sub(meanX)
     .square()
     .sum();
+
   const q = tf.scalar(t).mul(mse.div(sxx).sqrt());
   const c1 = b1.sub(q);
   const c2 = b1.add(q);
@@ -89,7 +92,52 @@ function confidenceInterval(t) {
   c2.print();
 }
 
-regressionLine();
-coefficienfOfDetermination();
-sampleCorrelation();
-confidenceInterval(2.228);
+// Confidence interval for an individual observation
+function predictionInterval(x, tScore) {
+  const y = regressionFunction(x);
+
+  const n = xData.shape[0];
+  const sxx = xData
+    .sub(meanX)
+    .square()
+    .sum();
+  const one = tf.scalar(1);
+
+  const s = sse.div(tf.scalar(n - 2)).sqrt();
+
+  const b = x
+    .sub(meanX)
+    .square()
+    .div(sxx);
+  const sa = one.add(one.div(n)).add(b);
+
+  const c1 = y.sub(tScore.mul(s).mul(sa.sqrt()));
+  const c2 = y.add(tScore.mul(s).mul(sa.sqrt()));
+  c1.print();
+  c2.print();
+}
+
+// Confidence interval for the mean difference at x
+function confidenceIntervalDifference(x, t) {
+  const y = regressionFunction(x);
+  const sxx = xData
+    .sub(meanX)
+    .square()
+    .sum();
+  const n = xData.shape[0];
+
+  const s = sse.div(tf.scalar(n - 2)).sqrt();
+
+  const b = x
+    .sub(meanX)
+    .square()
+    .div(sxx);
+  const a = tf.scalar(1 / n);
+  const c = a.add(b).sqrt();
+  const q = t.mul(s).mul(c);
+
+  const c1 = y.sub(q);
+  const c2 = y.add(q);
+  c1.print();
+  c2.print();
+}
